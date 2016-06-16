c89e9b1f9a2eb1b63626e8b76849f2dbd733d73c
batuo <batuo@alipay.com>
2016-06-16 16:05:22 +0800

    1

--- src/main/java/org/springframework/samples/mvc/simple/TokenController.java
+++ src/main/java/org/springframework/samples/mvc/simple/TokenController.java
@@ -1,33 +1,33 @@
-package org.springframework.samples.mvc.simple;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-
-@Controller
-@RequestMapping("/wechat")
-public class TokenController {
-    @Autowired
-    private TokenService tokenService;
-     
-    /**
-     * 开发者模式token校验
-     *
-     * @param wxAccount 开发者url后缀
-     * @param response
-     * @param tokenModel
-     * @throws ParseException
-     * @throws IOException
-     */
-    @RequestMapping(value = "/check/{wxToken}", method = RequestMethod.GET, produces = "text/plain")
-    public @ResponseBody String validate(@PathVariable("wxToken")String wxToken,CheckModel tokenModel) throws ParseException, IOException {
-        return tokenService.validate(wxToken,tokenModel);
-    }
-}
+//package org.springframework.samples.mvc.simple;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//
+//@Controller
+//@RequestMapping("/wechat")
+//public class TokenController {
+//    @Autowired
+//    private TokenService tokenService;
+//     
+//    /**
+//     * 开发者模式token校验
+//     *
+//     * @param wxAccount 开发者url后缀
+//     * @param response
+//     * @param tokenModel
+//     * @throws ParseException
+//     * @throws IOException
+//     */
+//    @RequestMapping(value = "/check/{wxToken}", method = RequestMethod.GET, produces = "text/plain")
+//    public @ResponseBody String validate(@PathVariable("wxToken")String wxToken,CheckModel tokenModel) throws ParseException, IOException {
+//        return tokenService.validate(wxToken,tokenModel);
+//    }
+//}
