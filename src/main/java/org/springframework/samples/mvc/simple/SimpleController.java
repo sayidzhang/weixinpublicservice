ce657efa89272a1abee8de27a4a059e7e0b9171c
batuo <batuo@alipay.com>
2016-06-16 15:55:28 +0800

    1

--- src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
+++ src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
@@ -1,15 +1,39 @@
 package org.springframework.samples.mvc.simple;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.willhunting.wechat.CheckModel;
+import org.willhunting.wechat.TokenService;
 
 @Controller
 public class SimpleController {
 
+    @Autowired
+    private TokenService tokenService;
+    
 	@RequestMapping("/simple")
 	public @ResponseBody String simple() {
 		return "Hello world!";
 	}
 
+    /**
+     * 开发者模式token校验
+     *
+     * @param wxAccount 开发者url后缀
+     * @param response
+     * @param tokenModel
+     * @throws ParseException
+     * @throws IOException
+     */
+    @RequestMapping(value = "wechat/check/{wxToken}", method = RequestMethod.GET, produces = "text/plain")
+    public @ResponseBody String validate(@PathVariable("wxToken")String wxToken,CheckModel tokenModel) throws ParseException, IOException {
+        return tokenService.validate(wxToken,tokenModel);
+    }
 }
