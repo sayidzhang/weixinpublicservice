00bd55481b1fc433cd21286cee3b82198ed8cb93
batuo <batuo@alipay.com>
2016-06-20 19:16:14 +0800

    1

---
+++ src/main/java/org/springframework/samples/mvc/simple/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.mvc.simple;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+ @Configuration
+ @EnableWebSecurity
+ public class SecurityConfig {
+
+    @Configuration
+    @Order(1)
+    public static class SoapApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/soap/**")
+                .csrf().disable()
+                .httpBasic();
+        }
+    }
+
+
+    @Configuration
+    public static class WebApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http        
+                .formLogin()
+                    .loginProcessingUrl("/authentication")
+                    .usernameParameter("j_username")
+                    .passwordParameter("j_password").permitAll()
+                    .and()
+                .csrf().disable();
+
+        }
+     }
+}
--- src/main/java/org/springframework/samples/mvc/simple/ServiceSecurityConfig.java
+++ src/main/java/org/springframework/samples/mvc/simple/ServiceSecurityConfig.java
@@ -1,16 +1,19 @@
-package org.springframework.samples.mvc.simple;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class ServiceSecurityConfig extends WebSecurityConfigurerAdapter {
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http.csrf().disable();
-	  }
-	}
-
-
-
+//package org.springframework.samples.mvc.simple;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class ServiceSecurityConfig extends WebSecurityConfigurerAdapter {
+//	  @Override
+//	  protected void configure(HttpSecurity http) throws Exception {
+//		  System.out.println("disable csrf...");
+//		  http.csrf().disable();
+//	  }
+//	}
+//
+//
+//
