9bdfa158f33c072e68473bd8116e096cc8043160
batuo <batuo@alipay.com>
2016-06-20 18:54:06 +0800

    1

--- src/main/java/org/springframework/samples/mvc/simple/ServiceSecurityConfig.java
+++ src/main/java/org/springframework/samples/mvc/simple/ServiceSecurityConfig.java
@@ -1,5 +1,16 @@
 package org.springframework.samples.mvc.simple;
 
-public class ServiceSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-}
+@Configuration
+public class ServiceSecurityConfig extends WebSecurityConfigurerAdapter {
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.csrf().disable();
+	  }
+	}
+
+
+
