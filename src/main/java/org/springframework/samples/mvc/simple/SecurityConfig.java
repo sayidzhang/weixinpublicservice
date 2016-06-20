773acbe6fbb7e290bb4be1608e7ed3f4031a50f0
batuo <batuo@alipay.com>
2016-06-20 19:35:56 +0800

    1

--- src/main/java/org/springframework/samples/mvc/simple/SecurityConfig.java
+++ src/main/java/org/springframework/samples/mvc/simple/SecurityConfig.java
@@ -1,41 +1,41 @@
-package org.springframework.samples.mvc.simple;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
- @Configuration
- @ComponentScan
- @EnableWebSecurity
- public class SecurityConfig {
-
-	@Order(1)
-    @Configuration
-    public static class SoapApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                .antMatcher("/soap/**")
-                .csrf().disable()
-                .httpBasic();
-        }
-    }
-
-	@Order(2)
-    @Configuration
-    public static class WebApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
-
-        protected void configure(HttpSecurity http) throws Exception {
-            http        
-                .formLogin()
-                    .loginProcessingUrl("/authentication")
-                    .usernameParameter("j_username")
-                    .passwordParameter("j_password").permitAll()
-                    .and()
-                .csrf().disable();
-
-        }
-     }
-}
+//package org.springframework.samples.mvc.simple;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+// @Configuration
+// @ComponentScan
+// @EnableWebSecurity
+// public class SecurityConfig {
+//
+//	@Order(1)
+//    @Configuration
+//    public static class SoapApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                .antMatcher("/soap/**")
+//                .csrf().disable()
+//                .httpBasic();
+//        }
+//    }
+//
+//	@Order(2)
+//    @Configuration
+//    public static class WebApiConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http        
+//                .formLogin()
+//                    .loginProcessingUrl("/authentication")
+//                    .usernameParameter("j_username")
+//                    .passwordParameter("j_password").permitAll()
+//                    .and()
+//                .csrf().disable();
+//
+//        }
+//     }
+//}
